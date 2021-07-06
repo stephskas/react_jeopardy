9a132a7d1d9ab13087571caac3df33dd3f849746
STEPH SAKAI <stephaniesakai@stephanies-mbp.attlocal.net>
2021-07-06 14:06:49 -0700

    converted react jeopardy into joke app

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Card } from "../card/card.component"
+import Card from "../card/card.component"
 import './card-list.styles.css'
 
 export const CardList = (props) => (
   <div className="card-list">
     {console.log(props)}
     {props.results.map((result) => (
-      <Card key={result.id} id={result.id} question={result.setup} answer={result.punchline} handleClick={props.handleClick} isAnswer={props}/>
+      <Card key={result.id} id={result.id} question={result.setup} answer={result.punchline} handleClick={props.handleClick} isAnswer={props.isAnswer}/>
     ))}
   </div>
 )
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,53 @@
- import React from 'react';
- import './card.styles.css'
+import React, { Component } from 'react';
+import './card.styles.css'
 
 let answer = "ANSWER"
-export const Card = (props) => (
-  <div className="card">
-    <p>{props.question}</p>
-    <button onClick={() => props.handleClick(props.id)} className="cardBtn">
-      {answer}
-    </button>
-    <p
-      style={{
-        display: props.isAnswer ? "block" : "none",
-        color: "#d4d7d8",
-        fontStyle: "italic",
-      }}
-    >
-      {props.answer}
-    </p>
-  </div>
-)
+
+class Card extends Component {
+  constructor(props){
+    super()
+    this.state ={
+      props: props    
+    }
+  }
+
+  render(){
+    return(
+      <div className="card">
+        <p>{this.state.props.question}</p>
+        <button onClick={() => this.setState({isAnswer: !this.state.isAnswer})} className="cardBtn">
+          {answer}
+        </button>
+        <p
+          style={{
+            display: this.state.isAnswer ? "block" : "none",
+            color: "#d4d7d8",
+            fontStyle: "italic",
+          }}
+        >
+          {this.state.props.answer}
+        </p>
+      </div>
+    )
+  }
+}
+export default Card
+
+
+// export const Card = (props) => (
+//   <div className="card">
+//     <p>{props.question}</p>
+//     <button onClick={() => props.handleClick(props.id)} className="cardBtn">
+//       {answer}
+//     </button>
+//     <p
+//       style={{
+//         display: props.isAnswer ? "block" : "none",
+//         color: "#d4d7d8",
+//         fontStyle: "italic",
+//       }}
+//     >
+//       {props.answer}
+//     </p>
+//   </div>
+// )
